2e000870152f76ea56abe2abf9eebbbd6b8b87cc
kylemarx86 <kylemarx86@csu.fullerton.edu>
2017-12-01 13:29:37 -0800

    add tests for GET /todos.

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,9 +4,17 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+    text: 'first test todo'
+}, {
+    text: 'second test todo'
+}];
+
 // clears out todos before running because checking the length below depends on it
 beforeEach((done) => {
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -25,7 +33,7 @@ describe('POST /todos', () => {
                     return done(err);
                 }
 
-                Todo.find().then((todos) => {
+                Todo.find({text}).then((todos) => {
                     // expect to be 1, because only one was added (with assumption db is empty. see beforeEach above)
                     expect(todos.length).toBe(1);
                     // check the text of the todos
@@ -51,11 +59,23 @@ describe('POST /todos', () => {
                 }
 
                 Todo.find().then((todos) => {
-                    // length of todos should be 0
-                    expect(todos.length).toBe(0);
+                    // length of todos should be 2
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((e) => done(e));
             });
         
-    })
+    });
+});
+
+describe('GET /todos', () => {
+    it('should get all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done);
+    });
 });
