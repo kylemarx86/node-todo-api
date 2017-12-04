5590b76b62521e35c9111068b70e37afe51de88a
kylemarx86 <kylemarx86@csu.fullerton.edu>
2017-12-03 18:35:36 -0800

    add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 //libraries
 var express = require('express');
 var bodyParser = require('body-parser');
-
+const {ObjectID} = require('mongodb');
 //local
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -30,7 +30,28 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(err);
     });
-})
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    // validate id using isvalid 
+        // respond with 404 and send empty body
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    // findbyid
+    Todo.findById(id).then((todo) => {
+        // success - 
+        // if todo 200 and send back
+        // if no todo send back 404 and send empty body
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.status(200).send({todo});
+    }).catch((err) => res.status(400).send());
+    // error -- send back 400 send back nothing
+});
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
