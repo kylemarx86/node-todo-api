4fed512c18074e755e32f5861b92c111043474d7
kylemarx86 <kylemarx86@csu.fullerton.edu>
2017-12-06 17:46:43 -0800

    add POST /users/login route.

--- server/models/user.js
+++ server/models/user.js
@@ -73,6 +73,30 @@ UserSchema.statics.findByToken = function(token) {
     });
 };
 
+UserSchema.statics.findByCredentials = function(email, password){
+    var User = this;
+
+    return User.findOne({email}).then((user) => {
+        if(!user){
+            return Promise.reject();
+        }
+
+        return new Promise((resolve, reject) => {
+            // bcrypt.compare to compare pass and user.pass
+            // if the result is true = call resolve with user
+            // if the result is false = reject
+        
+            bcrypt.compare(password, user.password, (err, res) => {
+                if(res){
+                    resolve(user)
+                }else{
+                    reject();
+                }
+            });
+        });
+    });
+};
+
 UserSchema.pre('save', function(next){
     var user = this;
 
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const bcrypt = require('bcryptjs');
 
 //local
 var {mongoose} = require('./db/mongoose');
@@ -100,7 +101,7 @@ app.post('/users', (req, res) => {
         return user.generateAuthToken();
         // returns token
     }).then((token) => {
-        res.header('x-auth', token).send(user)
+        res.header('x-auth', token).send(user);
     }).catch((err) => {
         res.status(400).send(err);
     });
@@ -110,7 +111,19 @@ app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user)
 });
 
+// POST /users/login {email, password}
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
 
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send();
+    });
+
+});
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
